6d6dd7d8890de2a231effc7ce4d6bb4b8f7d027a
evshell18 <evshell18@gmail.com>
2017-08-28 16:12:18 -0400

    Fixed files list to include service directory.

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
         // list of files / patterns to load in the browser
         files: [
             { pattern: "src/domain/**/*.ts" },
+            { pattern: "src/service/**/*.ts" },
             { pattern: "test/**/*.ts" },
         ],
 
